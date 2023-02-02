3bed297601aff5076432f362228c1b6ca501e1de
aliya0505 <114345965+aliya0505@users.noreply.github.com>
2023-02-02 13:35:04 +0530

    Update UserSignUpForm.jsx

--- src/components/BookMyShow/UserLoginForm/UserSignUpForm.jsx
+++ src/components/BookMyShow/UserLoginForm/UserSignUpForm.jsx
@@ -1,1 +1,123 @@
 
+import React, { useState } from "react";
+import { BsShieldLock } from "react-icons/bs";
+
+export function UserSignUpForm(props) {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [userFlag, setUserFlag] = useState(false);
+  const [emailFlag, setEmailFlag] = useState(false);
+  const [passwordFlag, setPasswordFlag] = useState(false);
+  const [infoExist, setInfoExist] = useState(false);
+
+  const loginFunction = (e) => {
+    e.stopPropagation();
+    props.flagSet();
+  };
+
+  const userInputHandler = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const passwordInputHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const emailInputHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
+  let loginInfo;
+  if (localStorage.getItem("userInfo") === null) {
+    loginInfo = [];
+  } else {
+    loginInfo = JSON.parse(localStorage.getItem("userInfo"));
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setUserFlag(false);
+    setEmailFlag(false);
+    setPasswordFlag(false);
+    setInfoExist(false);
+
+    if (userName === "") {
+      setUserFlag(true);
+      return;
+    }
+
+    if (email === "") {
+      setEmailFlag(true);
+      return;
+    }
+
+    if (password === "") {
+      setPasswordFlag(true);
+      return;
+    }
+
+    if (userName !== "" && password !== "" && email !== "") {
+      const storeToLocal = {
+        userName,
+        email,
+        password,
+      };
+      
+      if (
+        loginInfo.some(
+          (info) =>
+            info.userName === storeToLocal.userName ||
+            info.email === storeToLocal.email
+        )
+      ) {
+        setInfoExist(true);
+        return;
+      } else {
+        loginInfo.push(storeToLocal);
+        localStorage.setItem("userInfo", JSON.stringify(loginInfo));
+      }
+    }
+    setUserName("");
+    setPassword("");
+    setEmail("");
+    props.flagSet();
+  };
+
+  return (
+    <div className="wraper-div">
+      <form onSubmit={submitHandler} className="login-form">
+        <h3 className="user-heading">Sign Up</h3>
+        {infoExist && (
+          <div className="warning">Username or email already exists</div>
+        )}
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          onChange={userInputHandler}
+          value={userName}
+        />
+        {userFlag && <div className="warning">Please Enter User Name</div>}
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          onChange={emailInputHandler}
+          value={email}
+        />
+        {emailFlag && <div className="warning">Please Enter User Name</div>}
+        <input
+          type="password"
+          placeholder="Enter a Password"
+          onChange={passwordInputHandler}
+          value={password}
+        />
+        {passwordFlag && <div className="warning">Please Enter User Name</div>}
+        <button type="submit">Sign Up</button>
+      </form>
+      <p className="account-check" onClick={loginFunction}>
+         LOGIN?
+      </p>
+    </div>
+  );
+}
