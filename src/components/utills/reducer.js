71c65ac891e777b22ab32623b39449b397d3e513
aliya0505 <114345965+aliya0505@users.noreply.github.com>
2023-02-02 13:44:38 +0530

    Update reducer.js

--- src/components/utills/reducer.js
+++ src/components/utills/reducer.js
@@ -1,1 +1,17 @@
 
+export let initialData = {
+    status : false,
+    name : "sing up"}
+
+export const reducer = (state,action) => {
+  switch (action.type) {
+    case "COMPLETE":
+      return {
+        ...state,
+        status : true,
+        name : action.data
+      };
+    default:
+      return state;
+  }
+};
