8342abd91a070598dffb07200eb9eecd0a32021e
aliya0505 <114345965+aliya0505@users.noreply.github.com>
2023-02-02 13:43:46 +0530

    Update apicalls.js

--- src/components/utills/apicalls.js
+++ src/components/utills/apicalls.js
@@ -1,1 +1,13 @@
 
+import axios from "axios";
+
+async function apicalls(url) {
+  try {
+    const res = await axios.get(url);
+  return res;
+  } catch (error) {
+    return true;
+  }
+}
+
+export default apicalls;
