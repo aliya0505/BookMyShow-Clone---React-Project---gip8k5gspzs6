08923eca874f51f8ed83771e76240f2f944e48b9
aliya0505 <114345965+aliya0505@users.noreply.github.com>
2023-02-02 14:03:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
 
+import React from "react";
+import "./styles/App.css";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Home } from "./Home";
+import { CheckOut } from "./components/CheckOut/CheckOut";
+import { NavBar } from "./components/BookMyShow/NavBar/NavBar";
+import { WishList } from "./components/WishList/WishList";
+
+function App() {
+  return (
+    <div id="main">
+    <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<Home />}  />
+        <Route path="/checkout" element={<><NavBar/><CheckOut/></> } />
+        <Route path="/wishlist" element={<><NavBar/><WishList/></>} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
