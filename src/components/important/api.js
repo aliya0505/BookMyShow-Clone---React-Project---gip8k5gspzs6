64894c07918e36bbbef59c5e3b136131b2b7418e
aliya0505 <114345965+aliya0505@users.noreply.github.com>
2023-02-02 13:40:10 +0530

    Update api.js

--- src/components/important/api.js
+++ src/components/important/api.js
@@ -1,1 +1,9 @@
 
+export const apiUrl = {
+    key : "384ede869ab1d7aaf83ec8415f2b0089",
+    base : "https://api.themoviedb.org/3/",
+    getGenres : "genre/movie/list",
+    nowPlaying : "movie/now_playing",
+    language : "language=en-US",
+    imageBase : "https://image.tmdb.org/t/p/w500"
+}
