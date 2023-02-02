7dafe08f1f4fc2e1494c2e470b68a552fae406bc
aliya0505 <114345965+aliya0505@users.noreply.github.com>
2023-02-02 13:05:39 +0530

    Update Genres.jsx

--- src/components/BookMyShow/Genres/Genres.jsx
+++ src/components/BookMyShow/Genres/Genres.jsx
@@ -1,1 +1,24 @@
 
+import React from 'react'
+import '../Genres/Genres.css'
+
+export function Genres(props) {
+    const unOrderList = (arr) => {
+      return <ul key="unorderedList">
+        {arr.map((item) => {
+          return <li onClick={() => {checkGenresCheck(item)}} key={'li'+item.id} id={item.id} >{item.name}</li>
+        })}
+      </ul>
+    }
+
+    const checkGenresCheck = (argGenres) => {
+      props.newApicall(argGenres);
+    } 
+
+  return (
+    <div className='content'>
+     <h1 className="GenHead">Genre</h1>
+      {props.apiData === null ? "" : unOrderList(props.apiData)}
+    </div>
+  )
+}
